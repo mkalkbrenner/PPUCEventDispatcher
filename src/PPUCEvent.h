ef96a66f8ebd15511ff5744c0655a93192b77f46
Markus Kalkbrenner <git@kalki.de>
2021-02-19 18:11:11 +0100

    added operator to compare events

--- src/PPUCEvent.h
+++ src/PPUCEvent.h
@@ -8,12 +8,14 @@
 #ifndef PPUCEVENT_h
 #define PPUCEVENT_h
 
-#define EVENT_SOURCE_ANY 42 // "*"
-#define EVENT_SOURCE_EVENT 69 // "E", common event from different system, like VPX, DOF, PUP
-#define EVENT_SOURCE_DMD 68 // "D"
-#define EVENT_SOURCE_SWITCH 87 // "W"
-#define EVENT_SOURCE_LIGHT 76 // "L", mainly playfield inserts
-#define EVENT_SOURCE_SOLENOID 83 // "S", includes flashers
+#define EVENT_SOURCE_ANY      42 // "*" PPUC
+#define EVENT_SOURCE_DMD      68 // "D" VPX/DOF/PUP
+#define EVENT_SOURCE_EVENT    69 // "E" VPX/DOF/PUP common event from different system, like
+#define EVENT_SOURCE_EFFECT   70 // "F" PPUC        event from running PPUCEffect
+#define EVENT_SOURCE_LIGHT    76 // "L" VPX/DOF/PUP lights, mainly playfield inserts
+#define EVENT_SOURCE_SOUND    79 // "O" PPUC
+#define EVENT_SOURCE_SOLENOID 83 // "S" VPX/DOF/PUP includes flashers
+#define EVENT_SOURCE_SWITCH   87 // "W" VPX/DOF/PUP
 
 struct PPUCEvent {
     char sourceId;
@@ -30,6 +32,10 @@ struct PPUCEvent {
         sourceId = sId;
         eventId = eId;
         value = v;
+    }
+
+    bool operator == (const PPUCEvent& other) const {
+        return sourceId == other.sourceId && eventId == other.eventId  && value == other.value;
     }
 };
 
